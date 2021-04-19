0239ed80faffbb141ea0488255efcd144ff0d5e1
IgorAvshtol <iharrwe@gmail.com>
2021-04-19 18:51:30 +0300

    add container dialogs

---
+++ src/redux/profile-reducer.tsx
@@ -0,0 +1,50 @@
+import {ActionsTypes, PostsType, ProfilePageType} from "./store";
+
+let initialState = {
+    posts: [
+        {id: 1, message: 'Hi, how are you', likesCount: 1},
+        {id: 2, message: "It's my first post", likesCount: 1},
+    ],
+    newPostText: ""
+}
+
+const profileReducer = (state: ProfilePageType = initialState, action: ActionsTypes) => {
+    switch (action.type) {
+        case "ADD-POST":
+            let newPost: PostsType = {
+                id: 5,
+                message: state.newPostText,
+                // message: action.newPostText,
+                likesCount: 0
+            }
+            state.posts.push(newPost)
+            state.newPostText = ""
+            break;
+        case "UPDATE-NEW-POST-TEXT":
+            state.newPostText = action.newText
+            break;
+    }
+
+    return state
+}
+
+
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
+export const addPostActionCreator = () => {
+    return {
+        type: ADD_POST,
+        // newPostText: ""
+    } as const
+}
+
+export const updateNewPostTextActionCreator = (newText: string) => {
+    return {
+        type: UPDATE_NEW_POST_TEXT,
+        newText: newText
+    } as const
+}
+
+
+export default profileReducer
