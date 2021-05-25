173e17f61c7a54d7c8c7f9f172181681370c9490
IgorAvshtol <iharrwe@gmail.com>
2021-05-25 03:03:43 +0300

    add Form

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -4,12 +4,14 @@ import messageReducer from "./message-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messagePage: messageReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 export type AppStateType = ReturnType<typeof reducers>
